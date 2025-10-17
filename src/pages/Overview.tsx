206fffde931d15d495b6caa553df1d06a4a9f298
Alex delacey <alexdelacey@Alexs-MacBook-Pro-2.local>
2025-10-17 21:47:53 +1100

    Minor cosmetic changes

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -9,12 +9,12 @@ export default function Overview() {
           <div className="space-y-4">
               <h1>Hi, I'm Alex 👋</h1>
               <p>I’m a Product manager by trade.</p>
-              <p>I currently work at PurchasePlus where I spend most of my time making our platform more streamlined and scalable with software integrations.</p>
+              <p>I currently work at PurchasePlus where I spend most of my time making our platform streamlined and scalable with software integrations.</p>
           </div>
           <img
             src={portraitImage}
             alt="Alex portrait"
-            className="w-24 h-24 md:w-64 md:h-64 rounded-full object-cover border border-zinc-700/50"
+            className="w-24 h-24 md:w-64 md:h-64 rounded-full object-cover border border-zinc-700/50 hover:scale-105 transition-transform duration-200 ease-out"
           />
       </section>
     </PageLayout>
